d475cde8de6998b4bbaefe363d985410e2cb60c0
Lorena Ramonda <lorena.ramonda@gmail.com>
2019-03-02 20:44:38 +0100

    add system tray retina icon support

--- src/plugins/systemTray.js
+++ src/plugins/systemTray.js
@@ -1,21 +1,19 @@
+/*global __static*/
 import path from 'path'
-const {Menu, Tray} = require('electron').remote
-const trayIcon = new Tray(path.join(__static,'static/imgs/logo.png'))
+const { Menu, Tray } = require('electron').remote
+const trayIcon = new Tray(path.join(__static, 'static/imgs/clock-off@2x.png'))
 
 const trayMenuTemplate = [
-	{
-		label: 'Time Tracker',
-		enabled: false
-	},
+  {
+    label: 'Time Tracker',
+    enabled: false
+  },
 
-	{
-		label: 'Issues',
-		click: function () {
-			console.log('Clicked on issues')
-		}
-	},
+  {
+    label: 'Issues',
+    click: function() {}
+  }
 ]
 
 const trayMenu = Menu.buildFromTemplate(trayMenuTemplate)
 trayIcon.setContextMenu(trayMenu)
-
