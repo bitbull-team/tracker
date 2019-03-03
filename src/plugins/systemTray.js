679a2d01132b605548779256121649761f0f61f6
Fabio Gollinucci <fbgolly@gmail.com>
2019-03-03 09:05:23 +0100

    Add show application button

--- src/plugins/systemTray.js
+++ src/plugins/systemTray.js
@@ -1,17 +1,21 @@
-const electron = require('electron')
+import Vue from 'vue'
+import electron from 'electron'
+
 const currentWindow = electron.remote.getCurrentWindow()
 const { Menu } = electron.remote
 const trayIcon = currentWindow.systemTray
 
 const trayMenuTemplate = [
   {
-    label: 'Time Tracker',
-    enabled: false
-  },
-
-  {
-    label: 'Issues test',
-    click: function() {}
+    label: 'Show application',
+    click: () => {
+      if(currentWindow.isVisible() === false){
+        currentWindow.show()
+      }
+      if(currentWindow.isFocused() === false){
+        currentWindow.focus()
+      }
+    }
   }
 ]
 
