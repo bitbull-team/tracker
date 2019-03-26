2848fb58ba18e0d6cd5afe1109fbb5dd3aee182d
Fabio Gollinucci <fbgolly@gmail.com>
2019-03-27 00:16:02 +0100

    Add icons, add travis pipeline

--- src/system/tray.js
+++ src/system/tray.js
@@ -1,6 +1,6 @@
 /*global __static*/
 const path = require('path')
 const { Tray } = require('electron')
-const trayIcon = new Tray(path.join(__static, 'img/icons/clock-off@2x.png'))
+const trayIcon = new Tray(path.join(__static, 'tray/clock-off.png'))
 
 module.exports = trayIcon
