890d9b14d80a23cec6152879d618ccda161dfa2e
Fabio Gollinucci <fbgolly@gmail.com>
2019-03-27 18:18:43 +0100

    Fix icons for retina

--- src/system/tray.js
+++ src/system/tray.js
@@ -1,6 +1,6 @@
 /*global __static*/
 const path = require('path')
 const { Tray } = require('electron')
-const trayIcon = new Tray(path.join(__static, 'tray/clock-off.png'))
+const trayIcon = new Tray(path.join(__static, 'tray/clock-off@2x.png'))
 
 module.exports = trayIcon
