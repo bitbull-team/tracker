0db7d037ea7c6f7b4f8d8d036732887e3b1731b5
Lorena Ramonda <lorena.ramonda@gmail.com>
2019-07-08 15:08:27 +0200

    ref. #9 folder cleaness, move all images under img

--- src/system/tray.js
+++ src/system/tray.js
@@ -1,6 +1,6 @@
 /*global __static*/
 const path = require('path')
 const { Tray } = require('electron')
-const trayIcon = new Tray(path.join(__static, 'tray/clock-off@2x.png'))
+const trayIcon = new Tray(path.join(__static, 'img/tray/clock-off@2x.png'))
 
 module.exports = trayIcon
